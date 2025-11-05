7ae9c2280f3d517e745d172c345d5a5d4f481492
andre-verce <andrea.vercellino.13-2@accademiadigitaleliguria.it>
2025-11-05 09:42:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def carica_dati(file_path):
 # Carica i dati
 dati = carica_dati('dati.yaml')
 
-st.title("Alcuni concetti relativi al mondo delle telecomunicazioni Mission Critical")
-st.markdown("### Made with love by £&^!!$")
+st.title("Alcuni concetti relativi al mondo delle comunicazioni Mission Critical")
+st.markdown("### Clicca per espandere")
 
 # Itera sugli elementi e crea un expander per ciascuno
 for elemento in dati['elementi']:
